fc54dc44b3a8b7925adc48d1c608dc12dc35ecc0
mfsmillie <mfsmillie@gmail.com>
2026-09-23 08:59:35 -0400

    Update obesity care image

--- src/components/ObesityCare.tsx
+++ src/components/ObesityCare.tsx
@@ -17,7 +17,7 @@ export default function ObesityCare() {
 
         <div className="mt-14 flex justify-center">
           <img
-            src="/images/obesity-care/personal.png"
+            src="/images/lifestyle2.png"
             alt="A personalized obesity care plan showing Medical therapy, Lifestyle Support, Stomach remodeling, and Bariatric surgery"
             className="w-full max-w-2xl"
           />
